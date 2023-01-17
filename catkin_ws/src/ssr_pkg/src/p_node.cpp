e40eb365ff4b1d227d863f2df9447817eef5c50b
MIA <xiaoyazhou@ucsb.edu>
2023-01-17 13:07:36 -0800

    publisher created

--- catkin_ws/src/ssr_pkg/src/p_node.cpp
+++ catkin_ws/src/ssr_pkg/src/p_node.cpp
@@ -1,11 +1,22 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char  *argv[])
 {
     ros::init(argc, argv, "p_node");
     printf("hello from p_node\n");
+    
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<std_msgs::String>("topic_1",10);
+
+    ros::Rate loop_rate(10);
+
     while(ros::ok()){
         printf("operating p_node\n");
+        std_msgs::String msg;
+        msg.data = "sending msg from p_node";
+        pub.publish(msg);
+        loop_rate.sleep();
     }
     return 0;
 }
